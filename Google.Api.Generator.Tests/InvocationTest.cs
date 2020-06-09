1b73d78014a835fd3f388d860709b636f7d6f786
Chris Bacon <chris@dunelm.org.uk>
2020-06-09 17:34:13 +0100

    Support proto3 field presence; update protoc to 3.12.3

--- Google.Api.Generator.Tests/InvocationTest.cs
+++ Google.Api.Generator.Tests/InvocationTest.cs
@@ -32,7 +32,7 @@ namespace Google.Api.Generator.Tests
             var protoFilePath = Path.Combine(Invoker.GeneratorTestsDir, "ProtoTest.proto");
             using (Invoker.WithPath desc = Invoker.TempFile(), output = Invoker.TempDir())
             {
-                Invoker.Protoc($"-o {desc} --include_imports --include_source_info -I{Invoker.GeneratorTestsDir} {protoFilePath}");
+                Invoker.Protoc($"-o {desc} --experimental_allow_proto3_optional --include_imports --include_source_info -I{Invoker.GeneratorTestsDir} {protoFilePath}");
                 Invoker.Dotnet($"run -- --descriptor={desc} --package=testing --output={output}");
                 // Expect at least 6 generated files: Client, tests, and snippets - all with a .cs and a .csproj
                 var files = Directory.GetFiles(output.Path, "*", SearchOption.AllDirectories);
@@ -49,7 +49,7 @@ namespace Google.Api.Generator.Tests
             var protoFilePath = Path.Combine(Invoker.GeneratorTestsDir, "ProtoTest.proto");
             using (var output = Invoker.TempDir())
             {
-                Invoker.Protoc($"--plugin=protoc-gen-test={Invoker.PluginFile} --test_out={output} -I{Invoker.GeneratorTestsDir} {protoFilePath}");
+                Invoker.Protoc($"--experimental_allow_proto3_optional --plugin=protoc-gen-test={Invoker.PluginFile} --test_out={output} -I{Invoker.GeneratorTestsDir} {protoFilePath}");
                 // Expect at least 6 generated files: Client, tests, and snippets - all with a .cs and a .csproj
                 var files = Directory.GetFiles(output.Path, "*", SearchOption.AllDirectories);
                 Assert.True(files.Length >= 6, $"{files.Length} generated files present in output directory; must be >= 6");
---
+++ Google.Api.Generator.Tests/ProtoTests/OptionalFields/Testing.OptionalFields/OptionalFieldsResourceNames.g.cs
@@ -0,0 +1,228 @@
+﻿// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Generated code. DO NOT EDIT!
+
+using gax = Google.Api.Gax;
+using sys = System;
+using to = Testing.OptionalFields;
+
+namespace Testing.OptionalFields
+{
+    /// <summary>Resource name for the <c>Resource</c> resource.</summary>
+    public sealed partial class ResourceName : gax::IResourceName, sys::IEquatable<ResourceName>
+    {
+        /// <summary>The possible contents of <see cref="ResourceName"/>.</summary>
+        public enum ResourceNameType
+        {
+            /// <summary>An unparsed resource name.</summary>
+            Unparsed = 0,
+
+            /// <summary>A resource name with pattern <c>items/{item_id}</c>.</summary>
+            Item = 1
+        }
+
+        private static gax::PathTemplate s_item = new gax::PathTemplate("items/{item_id}");
+
+        /// <summary>Creates a <see cref="ResourceName"/> containing an unparsed resource name.</summary>
+        /// <param name="unparsedResourceName">The unparsed resource name. Must not be <c>null</c>.</param>
+        /// <returns>
+        /// A new instance of <see cref="ResourceName"/> containing the provided <paramref name="unparsedResourceName"/>
+        /// .
+        /// </returns>
+        public static ResourceName FromUnparsed(gax::UnparsedResourceName unparsedResourceName) =>
+            new ResourceName(ResourceNameType.Unparsed, gax::GaxPreconditions.CheckNotNull(unparsedResourceName, nameof(unparsedResourceName)));
+
+        /// <summary>Creates a <see cref="ResourceName"/> with the pattern <c>items/{item_id}</c>.</summary>
+        /// <param name="itemId">The <c>Item</c> ID. Must not be <c>null</c> or empty.</param>
+        /// <returns>A new instance of <see cref="ResourceName"/> constructed from the provided ids.</returns>
+        public static ResourceName FromItem(string itemId) =>
+            new ResourceName(ResourceNameType.Item, itemId: gax::GaxPreconditions.CheckNotNullOrEmpty(itemId, nameof(itemId)));
+
+        /// <summary>
+        /// Formats the IDs into the string representation of this <see cref="ResourceName"/> with pattern
+        /// <c>items/{item_id}</c>.
+        /// </summary>
+        /// <param name="itemId">The <c>Item</c> ID. Must not be <c>null</c> or empty.</param>
+        /// <returns>
+        /// The string representation of this <see cref="ResourceName"/> with pattern <c>items/{item_id}</c>.
+        /// </returns>
+        public static string Format(string itemId) => FormatItem(itemId);
+
+        /// <summary>
+        /// Formats the IDs into the string representation of this <see cref="ResourceName"/> with pattern
+        /// <c>items/{item_id}</c>.
+        /// </summary>
+        /// <param name="itemId">The <c>Item</c> ID. Must not be <c>null</c> or empty.</param>
+        /// <returns>
+        /// The string representation of this <see cref="ResourceName"/> with pattern <c>items/{item_id}</c>.
+        /// </returns>
+        public static string FormatItem(string itemId) =>
+            s_item.Expand(gax::GaxPreconditions.CheckNotNullOrEmpty(itemId, nameof(itemId)));
+
+        /// <summary>Parses the given resource name string into a new <see cref="ResourceName"/> instance.</summary>
+        /// <remarks>
+        /// To parse successfully, the resource name must be formatted as one of the following:
+        /// <list type="bullet"><item><description><c>items/{item_id}</c></description></item></list>
+        /// </remarks>
+        /// <param name="resourceName">The resource name in string form. Must not be <c>null</c>.</param>
+        /// <returns>The parsed <see cref="ResourceName"/> if successful.</returns>
+        public static ResourceName Parse(string resourceName) => Parse(resourceName, false);
+
+        /// <summary>
+        /// Parses the given resource name string into a new <see cref="ResourceName"/> instance; optionally allowing an
+        /// unparseable resource name.
+        /// </summary>
+        /// <remarks>
+        /// To parse successfully, the resource name must be formatted as one of the following:
+        /// <list type="bullet"><item><description><c>items/{item_id}</c></description></item></list>
+        /// Or may be in any format if <paramref name="allowUnparsed"/> is <c>true</c>.
+        /// </remarks>
+        /// <param name="resourceName">The resource name in string form. Must not be <c>null</c>.</param>
+        /// <param name="allowUnparsed">
+        /// If <c>true</c> will successfully store an unparseable resource name into the <see cref="UnparsedResource"/>
+        /// property; otherwise will throw an <see cref="sys::ArgumentException"/> if an unparseable resource name is
+        /// specified.
+        /// </param>
+        /// <returns>The parsed <see cref="ResourceName"/> if successful.</returns>
+        public static ResourceName Parse(string resourceName, bool allowUnparsed) =>
+            TryParse(resourceName, allowUnparsed, out ResourceName result) ? result : throw new sys::ArgumentException("The given resource-name matches no pattern.");
+
+        /// <summary>
+        /// Tries to parse the given resource name string into a new <see cref="ResourceName"/> instance.
+        /// </summary>
+        /// <remarks>
+        /// To parse successfully, the resource name must be formatted as one of the following:
+        /// <list type="bullet"><item><description><c>items/{item_id}</c></description></item></list>
+        /// </remarks>
+        /// <param name="resourceName">The resource name in string form. Must not be <c>null</c>.</param>
+        /// <param name="result">
+        /// When this method returns, the parsed <see cref="ResourceName"/>, or <c>null</c> if parsing failed.
+        /// </param>
+        /// <returns><c>true</c> if the name was parsed successfully; <c>false</c> otherwise.</returns>
+        public static bool TryParse(string resourceName, out ResourceName result) => TryParse(resourceName, false, out result);
+
+        /// <summary>
+        /// Tries to parse the given resource name string into a new <see cref="ResourceName"/> instance; optionally
+        /// allowing an unparseable resource name.
+        /// </summary>
+        /// <remarks>
+        /// To parse successfully, the resource name must be formatted as one of the following:
+        /// <list type="bullet"><item><description><c>items/{item_id}</c></description></item></list>
+        /// Or may be in any format if <paramref name="allowUnparsed"/> is <c>true</c>.
+        /// </remarks>
+        /// <param name="resourceName">The resource name in string form. Must not be <c>null</c>.</param>
+        /// <param name="allowUnparsed">
+        /// If <c>true</c> will successfully store an unparseable resource name into the <see cref="UnparsedResource"/>
+        /// property; otherwise will throw an <see cref="sys::ArgumentException"/> if an unparseable resource name is
+        /// specified.
+        /// </param>
+        /// <param name="result">
+        /// When this method returns, the parsed <see cref="ResourceName"/>, or <c>null</c> if parsing failed.
+        /// </param>
+        /// <returns><c>true</c> if the name was parsed successfully; <c>false</c> otherwise.</returns>
+        public static bool TryParse(string resourceName, bool allowUnparsed, out ResourceName result)
+        {
+            gax::GaxPreconditions.CheckNotNull(resourceName, nameof(resourceName));
+            gax::TemplatedResourceName resourceName2;
+            if (s_item.TryParseName(resourceName, out resourceName2))
+            {
+                result = FromItem(resourceName2[0]);
+                return true;
+            }
+            if (allowUnparsed)
+            {
+                if (gax::UnparsedResourceName.TryParse(resourceName, out gax::UnparsedResourceName unparsedResourceName))
+                {
+                    result = FromUnparsed(unparsedResourceName);
+                    return true;
+                }
+            }
+            result = null;
+            return false;
+        }
+
+        private ResourceName(ResourceNameType type, gax::UnparsedResourceName unparsedResourceName = null, string itemId = null)
+        {
+            Type = type;
+            UnparsedResource = unparsedResourceName;
+            ItemId = itemId;
+        }
+
+        /// <summary>
+        /// Constructs a new instance of a <see cref="ResourceName"/> class from the component parts of pattern
+        /// <c>items/{item_id}</c>
+        /// </summary>
+        /// <param name="itemId">The <c>Item</c> ID. Must not be <c>null</c> or empty.</param>
+        public ResourceName(string itemId) : this(ResourceNameType.Item, itemId: gax::GaxPreconditions.CheckNotNullOrEmpty(itemId, nameof(itemId)))
+        {
+        }
+
+        /// <summary>The <see cref="ResourceNameType"/> of the contained resource name.</summary>
+        public ResourceNameType Type { get; }
+
+        /// <summary>
+        /// The contained <see cref="gax::UnparsedResourceName"/>. Only non-<c>null</c> if this instance contains an
+        /// unparsed resource name.
+        /// </summary>
+        public gax::UnparsedResourceName UnparsedResource { get; }
+
+        /// <summary>
+        /// The <c>Item</c> ID. Will not be <c>null</c>, unless this instance contains an unparsed resource name.
+        /// </summary>
+        public string ItemId { get; }
+
+        /// <summary>Whether this instance contains a resource name with a known pattern.</summary>
+        public bool IsKnownPattern => Type != ResourceNameType.Unparsed;
+
+        /// <summary>The string representation of the resource name.</summary>
+        /// <returns>The string representation of the resource name.</returns>
+        public override string ToString()
+        {
+            switch (Type)
+            {
+                case ResourceNameType.Unparsed: return UnparsedResource.ToString();
+                case ResourceNameType.Item: return s_item.Expand(ItemId);
+                default: throw new sys::InvalidOperationException("Unrecognized resource-type.");
+            }
+        }
+
+        /// <summary>Returns a hash code for this resource name.</summary>
+        public override int GetHashCode() => ToString().GetHashCode();
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj) => Equals(obj as ResourceName);
+
+        /// <inheritdoc/>
+        public bool Equals(ResourceName other) => ToString() == other?.ToString();
+
+        /// <inheritdoc/>
+        public static bool operator ==(ResourceName a, ResourceName b) => ReferenceEquals(a, b) || (a?.Equals(b) ?? false);
+
+        /// <inheritdoc/>
+        public static bool operator !=(ResourceName a, ResourceName b) => !(a == b);
+    }
+
+    public partial class Request
+    {
+        /// <summary>
+        /// <see cref="to::ResourceName"/>-typed view over the <see cref="Name"/> resource name property.
+        /// </summary>
+        public to::ResourceName ResourceName
+        {
+            get => string.IsNullOrEmpty(Name) ? null : to::ResourceName.Parse(Name, allowUnparsed: true);
+            set => Name = value?.ToString() ?? "";
+        }
+    }
+}
