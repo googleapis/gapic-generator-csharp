29091a5a0a14dee76baaeaea3d245307bcb6e265
Jon Skeet <jonskeet@google.com>
2020-08-19 15:29:06 +0100

    First pass at actual code generation
    Obviously there's a long way to go, but this does actually generate some code.

--- Google.Api.Generator.Rest/CodeGenerator.cs
+++ Google.Api.Generator.Rest/CodeGenerator.cs
@@ -12,8 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Google.Api.Gax;
+using Google.Api.Generator.Rest.Models;
 using Google.Api.Generator.Utils;
+using Google.Apis.Discovery.v1.Data;
+using Google.Apis.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static Google.Api.Generator.Utils.Roslyn.RoslynBuilder;
 
 namespace Google.Api.Generator.Rest
 {
@@ -21,7 +29,64 @@ namespace Google.Api.Generator.Rest
     {
         public static IEnumerable<ResultFile> Generate(string discoveryJson)
         {
-            yield break;
+            var discoveryDescription = NewtonsoftJsonSerializer.Instance.Deserialize<RestDescription>(discoveryJson);
+            var package = new PackageModel(discoveryDescription);
+            yield return GenerateCSharpCode(package);
+            yield return GenerateProjectFile(package);
+            yield return GenerateNet40Config();
+            yield return GenerateNetStandard10Config();
+        }
+
+        private static ResultFile GenerateCSharpCode(PackageModel package)
+        {
+            var ctx = SourceFileContext.CreateFullyQualified(SystemClock.Instance);
+            var ns = Namespace(package.PackageName);
+            using (ctx.InNamespace(ns))
+            {
+                var serviceClass = package.GenerateServiceClass(ctx);
+                var baseRequestClass = package.GenerateBaseRequestClass(ctx);
+                ns = ns.AddMembers(serviceClass, baseRequestClass);
+
+                foreach (var resource in package.Resources)
+                {
+                    var resourceClass = resource.GenerateClass(ctx);
+                    ns = ns.AddMembers(resourceClass);
+                }
+            }
+            var dataNs = Namespace(package.PackageName + ".Data");
+            using (ctx.InNamespace(dataNs))
+            {
+                foreach (var dataModel in package.DataModels.Where(dm => dm.Parent is null))
+                {
+                    dataNs = dataNs.AddMembers(dataModel.GenerateClass(ctx));
+                }
+            }
+            var syntax = ctx.CreateCompilationUnit(ns).AddMembers(dataNs);
+            return new ResultFile($"{package.PackageName}.cs", syntax);
+        }
+
+        private static ResultFile GenerateProjectFile(PackageModel package)
+        {
+            var doc = package.GenerateProjectFile();
+            return new ResultFile($"{package.PackageName}.csproj", doc.ToString());
+        }
+
+        private static ResultFile GenerateNet40Config() =>
+            new ResultFile("app.net40.config", LoadResource("Net40Config.xml"));
+
+        private static ResultFile GenerateNetStandard10Config() =>
+            new ResultFile("app.netstandard10.config", LoadResource("NetStandard10Config.xml"));
+
+        private static string LoadResource(string name)
+        {
+            string resource = "Google.Api.Generator.Rest.StaticResources." + name;
+            using var stream = typeof(CodeGenerator).Assembly.GetManifestResourceStream(resource);
+            if (stream is null)
+            {
+                var allResourceNames = typeof(CodeGenerator).Assembly.GetManifestResourceNames();
+                throw new ArgumentException($"Unable to find resource '{resource}'. Available resources: {string.Join(",", allResourceNames)}");
+            }
+            return new StreamReader(stream).ReadToEnd();
         }
     }
 }
