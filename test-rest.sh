44a216288a46bdba41727ab94c06f335b89d37db
Jon Skeet <jonskeet@google.com>
2020-09-09 08:10:01 +0100

    Add script to help REST generator development

---
+++ test-rest.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Simple script to run the generator on the Translate and Webfonts APIs
+# This is simply to aid development of the REST generator.
+
+dotnet run -p Google.Api.Generator.Rest Google.Api.Generator.Rest.Tests/GoldenTestData/Google.Apis.Translate.v2/discovery.json tmp
+dotnet run --no-build -p Google.Api.Generator.Rest Google.Api.Generator.Rest.Tests/GoldenTestData/Google.Apis.Webfonts.v1/discovery.json tmp
