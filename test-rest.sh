4f5d87d77fe4e5c836084d560d6637b9426c5994
Jon Skeet <jonskeet@google.com>
2020-09-10 10:18:02 +0100

    Add Storage as a test service, because it contains nested resources
    (It also has a lot of other relatively-unusual features like media upload... if Storage works, most other APIs are likely to be okay.)

--- test-rest.sh
+++ test-rest.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+set -e
+
 # Simple script to run the generator on the Translate and Webfonts APIs
 # This is simply to aid development of the REST generator.
 
 dotnet run -p Google.Api.Generator.Rest Google.Api.Generator.Rest.Tests/GoldenTestData/Google.Apis.Translate.v2/discovery.json tmp
 dotnet run --no-build -p Google.Api.Generator.Rest Google.Api.Generator.Rest.Tests/GoldenTestData/Google.Apis.Webfonts.v1/discovery.json tmp
+dotnet run --no-build -p Google.Api.Generator.Rest Google.Api.Generator.Rest.Tests/GoldenTestData/Google.Apis.Storage.v1/discovery.json tmp
