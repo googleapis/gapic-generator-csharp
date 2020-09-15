f4378cde23b7c5aa79fa06aaf075550cbc906237
Jon Skeet <jonskeet@google.com>
2020-09-15 10:20:26 +0100

    Hack the #if directives for the service
    We still need to do this for media download as well, but that will be it.

--- Google.Api.Generator.Rest/CodeGenerator.cs
+++ Google.Api.Generator.Rest/CodeGenerator.cs
@@ -15,8 +15,10 @@
 using Google.Api.Gax;
 using Google.Api.Generator.Rest.Models;
 using Google.Api.Generator.Utils;
+using Google.Api.Generator.Utils.Formatting;
 using Google.Apis.Discovery.v1.Data;
 using Google.Apis.Json;
+using Microsoft.CodeAnalysis;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
@@ -80,7 +82,42 @@ namespace Google.Api.Generator.Rest
                 }
             }
             var syntax = ctx.CreateCompilationUnit(ns).AddMembers(dataNs);
-            return new ResultFile($"{package.PackageName}.cs", syntax);
+            
+            string content = ApplyIfDirectives(CodeFormatter.Format(syntax).ToFullString());
+            return new ResultFile($"{package.PackageName}.cs", content);
+        }
+
+        /// <summary>
+        /// The #if directives in the service are relatively tricky to generate. We hack them in here...
+        /// </summary>
+        private static string ApplyIfDirectives(string code)
+        {
+            bool usesCarriageReturn = code.Contains('\r');
+            var lines = code.Replace("\r", "").Split('\n').ToList();
+
+            int baseUriIndex = lines.FindIndex(line => line.Contains("public override string BaseUri => BaseUriOverride ??"));
+            string withBaseUriOverrideExpression = lines[baseUriIndex].Split(" => ").Last();
+            string withoutBaseUriOverrideExpression = withBaseUriOverrideExpression.Split('?').Last().Trim();
+            lines[baseUriIndex] = lines[baseUriIndex].Split(" => ").First() + " =>";
+            lines.InsertRange(baseUriIndex + 1, new[]
+            {
+                "        #if NETSTANDARD1_3 || NETSTANDARD2_0 || NET45",
+                "            " + withBaseUriOverrideExpression,
+                "        #else",
+                "            " + withoutBaseUriOverrideExpression,
+                "        #endif"
+            });
+
+            int batchBaseUriIndex = lines.FindIndex(line => line.Contains("/// <summary>Gets the batch base URI; <c>null</c> if unspecified.</summary>"));
+            lines.Insert(batchBaseUriIndex, "        #if !NET40");
+
+            int batchPathIndex = lines.FindIndex(line => line.Contains("public override string BatchPath =>"));
+            lines.Insert(batchPathIndex + 1, "        #endif");
+
+            // TODO: Media downloads have #if as well, for the DownloadRange and DownloadRangeAsync methods.
+
+            string separator = usesCarriageReturn ? "\r\n" : "\n";
+            return string.Join(separator, lines);
         }
 
         private static ResultFile GenerateProjectFile(PackageModel package)
