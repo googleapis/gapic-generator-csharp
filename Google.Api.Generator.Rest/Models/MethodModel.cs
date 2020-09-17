f7b798392b371a3e917f3aa239017a60b2de9e3e
Jon Skeet <jonskeet@google.com>
2020-09-17 16:34:14 +0100

    Add media upload supprt

--- Google.Api.Generator.Rest/Models/MethodModel.cs
+++ Google.Api.Generator.Rest/Models/MethodModel.cs
@@ -17,6 +17,8 @@ using Google.Api.Generator.Utils.Roslyn;
 using Google.Apis.Discovery.v1.Data;
 using Google.Apis.Download;
 using Google.Apis.Services;
+using Google.Apis.Upload;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System.Collections.Generic;
 using System.IO;
@@ -39,7 +41,6 @@ namespace Google.Api.Generator.Rest.Models
         /// </summary>
         public ResourceModel Resource { get; }
         
-        public IReadOnlyList<ParameterModel> Parameters { get; }
         public Typ ParentTyp { get; }
         public Typ RequestTyp { get; }
         public Typ ResponseTyp { get; }
@@ -59,17 +60,15 @@ namespace Google.Api.Generator.Rest.Models
             RequestTyp = Typ.Nested(ParentTyp, $"{PascalCasedName}Request");
             BodyTyp = restMethod.Request is object ? package.GetDataModelByReference(restMethod.Request.Ref__).Typ : null;
             ResponseTyp = restMethod.Response is object ? package.GetDataModelByReference(restMethod.Response.Ref__).Typ : Typ.Of<string>();
-            Parameters = CreateParameterList(package, restMethod, RequestTyp);
             _restMethod = restMethod;
         }
 
-        private static IReadOnlyList<ParameterModel> CreateParameterList(PackageModel package, RestMethod restMethod, Typ parentTyp)
+        private IReadOnlyList<ParameterModel> CreateParameterList(Typ parentTyp)
         {
-            var parameterOrder = restMethod.ParameterOrder ?? new List<string>();
+            var parameterOrder = _restMethod.ParameterOrder ?? new List<string>();
             // TODO: Skip the Alt parameter
-            // TODO: Handle the body parameter if restMethod.Request is non-null
-            return (restMethod.Parameters ?? new Dictionary<string, JsonSchema>())
-                .Select(pair => new ParameterModel(package, pair.Key, pair.Value, parentTyp))
+            return (_restMethod.Parameters ?? new Dictionary<string, JsonSchema>())
+                .Select(pair => new ParameterModel(Package, pair.Key, pair.Value, parentTyp))
                 .OrderBy(p => !p.IsRequired)
                 .ThenBy(p => parameterOrder.IndexOf(p.Name))
                 .ToList()
@@ -80,12 +79,20 @@ namespace Google.Api.Generator.Rest.Models
         {
             yield return GenerateMethodDeclaration(ctx);
             yield return GenerateRequestType(ctx);
+            if (SupportsMediaUpload)
+            {
+                foreach (var member in GenerateUploadMembers(ctx))
+                {
+                    yield return member;
+                }
+            }
         }
 
         private MethodDeclarationSyntax GenerateMethodDeclaration(SourceFileContext ctx)
         {
+            var parameters = CreateParameterList(RequestTyp);
             var docs = new List<DocumentationCommentTriviaSyntax>();
-            var methodParameters = Parameters.TakeWhile(p => p.IsRequired).ToList();
+            var methodParameters = parameters.TakeWhile(p => p.IsRequired).ToList();
             var parameterDeclarations = methodParameters.Select(p => Parameter(ctx.Type(p.Typ), p.CodeParameterName)).ToList();
             if (_restMethod.Description is object)
             {
@@ -141,7 +148,8 @@ namespace Google.Api.Generator.Rest.Models
                         .WithXmlDoc(XmlDoc.Summary("Gets the media downloader."))
                     : null;
 
-                var requiredParameters = Parameters
+                var parameters = CreateParameterList(RequestTyp);
+                var requiredParameters = parameters
                     .TakeWhile(p => p.IsRequired)
                     .Select(p => (param: p, decl: Parameter(ctx.Type(p.Typ), p.CodeParameterName)))
                     .ToList();
@@ -181,13 +189,13 @@ namespace Google.Api.Generator.Rest.Models
                 var initParameters = Method(Modifier.Protected | Modifier.Override, VoidType, "InitParameters")()
                     .WithBlockBody(
                         BaseExpression().Call("InitParameters")(),
-                        Parameters.Select(p => p.GenerateInitializer(ctx)).ToArray())
+                        parameters.Select(p => p.GenerateInitializer(ctx)).ToArray())
                     .WithXmlDoc(XmlDoc.Summary($"Initializes {PascalCasedName} parameter list."));
 
                 // TODO: Media downloader members
 
                 cls = cls.AddMembers(ctor);
-                cls = cls.AddMembers(Parameters.SelectMany(p => p.GenerateDeclarations(ctx)).ToArray());
+                cls = cls.AddMembers(parameters.SelectMany(p => p.GenerateDeclarations(ctx)).ToArray());
                 cls = cls.AddMembers(bodyDeclarations);
                 cls = cls.AddMembers(methodName, httpMethod, restPath, initParameters);
 
@@ -195,11 +203,6 @@ namespace Google.Api.Generator.Rest.Models
                 {
                     cls = cls.AddMembers(mediaDownloaderProperty);
                     cls = AddMediaDownloadMethods(mediaDownloaderProperty, cls, ctx);
-                }
-
-                if (SupportsMediaUpload)
-                {
-                    cls = AddMediaUploadMethods(cls, ctx);
                 }
             }
             return cls;
@@ -253,10 +256,99 @@ namespace Google.Api.Generator.Rest.Models
             return cls.AddMembers(syncDownload, syncDownloadWithStatus, asyncDownloadNoToken, asyncDownloadWithToken, syncRange, asyncRange);
         }
 
-        private static ClassDeclarationSyntax AddMediaUploadMethods(ClassDeclarationSyntax cls, SourceFileContext ctx)
+        private IEnumerable<MemberDeclarationSyntax> GenerateUploadMembers(SourceFileContext ctx)
         {
-            // TODO: add the members
-            return cls;
+            var uploadTyp = Typ.Nested(ctx.CurrentTyp, PascalCasedName + "MediaUpload");
+
+            var baseTypArgument1 = BodyTyp ?? Typ.Of<string>();
+            var baseTyp = ResponseTyp is object
+                ? Typ.Generic(Typ.Of(typeof(ResumableUpload<,>)), baseTypArgument1, ResponseTyp)
+                : Typ.Generic(Typ.Of(typeof(ResumableUpload<>)), baseTypArgument1);
+
+            var uploadCls = Class(Modifier.Public, uploadTyp, ctx.Type(baseTyp))
+                .WithXmlDoc(XmlDoc.Summary($"{PascalCasedName} media upload which supports resumable upload."));
+
+            var parameters = CreateParameterList(uploadTyp);
+            var requiredParameters = parameters
+                .TakeWhile(p => p.IsRequired)
+                .Select(p => (param: p, decl: Parameter(ctx.Type(p.Typ), p.CodeParameterName)))
+                .ToList();
+
+            var insertMethodParameters = new List<ParameterSyntax>();
+            if (BodyTyp is object)
+            {
+                insertMethodParameters.Add(Parameter(ctx.Type(BodyTyp), "body"));
+            }
+            insertMethodParameters.AddRange(requiredParameters.Select(pair => pair.decl));
+            var streamParam = Parameter(ctx.Type<Stream>(), "stream");
+
+            var contentTypeParam = Parameter(ctx.Type<string>(), "contentType");
+            insertMethodParameters.Add(streamParam);
+            insertMethodParameters.Add(contentTypeParam);
+
+            // This doc comment is common between the method and the constructor.
+            var remarks = XmlDoc.Remarks("Considerations regarding ", streamParam, ":",
+                XmlDoc.UL(
+                    XmlDoc.Item("If ", streamParam, " is seekable, then the stream position will be reset to ", 0, " before reading commences. If ", streamParam, " is not seekable, then it will be read from its current position"),
+                    XmlDoc.Item("Caller is responsible for maintaining the ", streamParam, " open until the upload is completed"),
+                    XmlDoc.Item("Caller is responsible for closing the ", streamParam)));
+
+            var methodDocComments = new List<DocumentationCommentTriviaSyntax>
+            {
+                XmlDoc.Summary("Stores a new object and metadata"),
+                remarks
+            };
+            if (BodyTyp is object)
+            {
+                methodDocComments.Add(XmlDoc.Param(Parameter(ctx.Type(BodyTyp), "body"), "The body of the request."));
+            }
+
+            methodDocComments.AddRange(requiredParameters.Select(pair => XmlDoc.Param(pair.decl, pair.param.Description)));
+            methodDocComments.Add(XmlDoc.Param(streamParam, "The stream to upload. See remarks for further information."));
+            methodDocComments.Add(XmlDoc.Param(contentTypeParam, "The content type of the stream to upload."));
+
+            // We're taking it from the field in the parent type, but it becomes a parameter for the constructor...
+            // So long as it appears as "service" we don't really mind.
+            var serviceParam = Parameter(ctx.Type<IClientService>(), "service");
+            var ctorParameters = new List<ParameterSyntax> { serviceParam };
+            ctorParameters.AddRange(insertMethodParameters);
+
+            var insertMethod = Method(Modifier.Public | Modifier.Virtual, ctx.Type(uploadTyp), PascalCasedName)(insertMethodParameters.ToArray())
+                .WithBlockBody(Return(New(ctx.Type(uploadTyp))(ctorParameters)))
+                .WithXmlDoc(methodDocComments.ToArray());
+
+            using (ctx.InClass(uploadTyp))
+            {
+                var baseInitializerArgs = new List<object>
+                {
+                    serviceParam,
+                    ctx.Type<string>().Call(nameof(string.Format))("/{0}/{1}{2}", "upload", serviceParam.Access("BasePath"), _restMethod.Path),
+                    _restMethod.HttpMethod,
+                    streamParam,
+                    contentTypeParam
+                };
+                var baseInitializer = BaseInitializer(baseInitializerArgs).WithAdditionalAnnotations(Annotations.LineBreakAnnotation);
+                var assignments = requiredParameters.Select(p => Field(0, ctx.Type(p.param.Typ), p.param.PropertyName).Assign(p.decl)).ToList();
+
+                if (BodyTyp is object)
+                {
+                    // The Body property is in the base class.
+                    var bodyProperty = Property(Modifier.Public, ctx.Type(BodyTyp), "Body");
+                    assignments.Add(bodyProperty.Assign(Parameter(ctx.Type(BodyTyp), "body")));
+                }
+
+                // The parameters to the constructor are the same as for the method.
+                var uploadCtor = Ctor(Modifier.Public, uploadCls, baseInitializer)(ctorParameters.ToArray())
+                    .WithBlockBody(assignments)
+                    .WithXmlDoc(XmlDoc.Summary($"Constructs a new {PascalCasedName} media upload instance."), remarks);
+
+                uploadCls = uploadCls.AddMembers(Package.CreateParameterList(uploadTyp).SelectMany(p => p.GenerateDeclarations(ctx)).ToArray());
+                uploadCls = uploadCls.AddMembers(parameters.SelectMany(p => p.GenerateDeclarations(ctx)).ToArray());
+                uploadCls = uploadCls.AddMembers(uploadCtor);
+            }
+
+            yield return insertMethod;
+            yield return uploadCls;
         }
     }
 }
