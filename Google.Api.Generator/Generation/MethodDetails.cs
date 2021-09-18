f3f965a5b6fa26f8a4db2e27fefe16ad8c8c59c0
Viacheslav Rostovtsev <virost@google.com>
2021-09-18 16:02:44 -0700

    fixup

--- Google.Api.Generator/Generation/MethodDetails.cs
+++ Google.Api.Generator/Generation/MethodDetails.cs
@@ -24,7 +24,6 @@ using Grpc.ServiceConfig;
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Text;
