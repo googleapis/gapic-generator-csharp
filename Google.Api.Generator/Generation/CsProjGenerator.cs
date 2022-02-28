271a0ab8c7f19172c67c886f648a065adad8b131
Jon Skeet <jonskeet@google.com>
2022-02-28 11:09:49 +0000

    chore: Update Moq dependency to 4.17.1
    Supercedes #450

--- Google.Api.Generator/Generation/CsProjGenerator.cs
+++ Google.Api.Generator/Generation/CsProjGenerator.cs
@@ -32,7 +32,7 @@ namespace Google.Api.Generator.Generation
         private const string TestSdkVersion = "17.1.0";
         private const string XUnitRunnerVersion = "2.4.3";
         private const string XUnitVersion = "2.4.1";
-        private const string MoqVersion = "4.16.1";
+        private const string MoqVersion = "4.17.1";
         private static readonly Dictionary<string, (string, string)> MixinToPackageAndVersion = new Dictionary<string, (string, string)>
         {
             { IAMPolicy.Descriptor.FullName, (typeof(IAMPolicyClient).Namespace, IamVersion) },
