12c72d29e5101dbe15ca827f0f23e08b3d97c2ef
Jon Skeet <jonskeet@google.com>
2022-05-05 15:36:00 +0100

    chore: Update xunit.runner.visualstudio to v2.4.4

--- Google.Api.Generator/Generation/CsProjGenerator.cs
+++ Google.Api.Generator/Generation/CsProjGenerator.cs
@@ -30,7 +30,7 @@ namespace Google.Api.Generator.Generation
         private const string ReferenceAssembliesVersion = "1.0.2";
         private const string SystemLinqAsyncVersion = "6.0.1";
         private const string TestSdkVersion = "17.1.0";
-        private const string XUnitRunnerVersion = "2.4.3";
+        private const string XUnitRunnerVersion = "2.4.4";
         private const string XUnitVersion = "2.4.1";
         private const string MoqVersion = "4.17.2";
         private static readonly Dictionary<string, (string, string)> MixinToPackageAndVersion = new Dictionary<string, (string, string)>
