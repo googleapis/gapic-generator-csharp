377ed2dc62775eb1b49d2b7cbf532aed1b08aca6
Jon Skeet <jonskeet@google.com>
2022-05-25 14:38:28 +0100

    chore: Update Grpc.Core dependency to 2.46.3

--- Google.Api.Generator/Generation/CsProjGenerator.cs
+++ Google.Api.Generator/Generation/CsProjGenerator.cs
@@ -23,7 +23,7 @@ namespace Google.Api.Generator.Generation
     internal static class CsProjGenerator
     {
         private const string GaxGrpcVersion = "[4.0.0, 5.0.0)";
-        private const string GrpcCoreVersion = "[2.41.0, 3.0.0)";
+        private const string GrpcCoreVersion = "[2.46.3, 3.0.0)";
         private const string LroVersion = "[3.0.0, 4.0.0)";
         private const string IamVersion = "[3.0.0, 4.0.0)";
         private const string LocationVersion = "[2.0.0, 3.0.0)";
