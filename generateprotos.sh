20d8d94eb9a9d45c2b09dbb65aa8055c9a95dee2
Jon Skeet <jonskeet@google.com>
2022-09-07 10:44:22 +0100

    tools: Add scripts for updating protoc/gRPC binaries, and generating all protos
    The generateprotos.sh script replaces the previous directory-specific scripts.

---
+++ generateprotos.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Uses protoc and the gRPC plugin in tools to generate
+# all the protos we use:
+#
+# - protoc compiler plugin
+# - Generator config protos (e.g. service config, snippets)
+# - GAPIC ShowCase
+#
+# Currently this script is hard-coded to use protoc.exe,
+# so it only runs on Windows; we can use platform detection
+# if that ever becomes an issue.
+
+
+# Protoc compiler plugin
+# This will become obsolete as of protobuf 3.22,
+# when the classes will be in Google.Protobuf.
+echo "Generating protoc compiler plugin"
+tools/protoc.exe \
+  --csharp_out=Google.Api.Generator/Google.Protobuf.Compiler \
+  -Itools/protos \
+  -IGoogle.Api.Generator/Google.Protobuf.Compiler \
+  Google.Api.Generator/Google.Protobuf.Compiler/plugin.proto
+
+# Generator config protos
+echo "Generating generator config protos"
+tools/protoc.exe \
+  --csharp_out=Google.Api.Generator/ConfigProtos \
+  -Itools/protos \
+  -Igoogleapis \
+  -IGoogle.Api.Generator/ConfigProtos \
+  Google.Api.Generator/ConfigProtos/*.proto
+
+# GAPIC Showcase
+echo "Generating GAPIC Showcase"
+tools/protoc.exe \
+  --csharp_out=Google.Api.Generator.Tests/ProtoTests/Showcase \
+  --csharp_opt=file_extension=.g.cs \
+  --plugin=protoc-gen-grpc=tools/grpc_csharp_plugin.exe \
+  --grpc_out=Google.Api.Generator.Tests/ProtoTests/Showcase \
+  --grpc_opt=file_suffix=Grpc.g.cs \
+  -Itools/protos \
+  -Igoogleapis \
+  -IGoogle.Api.Generator.Tests/ProtoTests/Showcase \
+  Google.Api.Generator.Tests/ProtoTests/Showcase/google/showcase/v1beta1/*.proto
+
+echo "Done"
