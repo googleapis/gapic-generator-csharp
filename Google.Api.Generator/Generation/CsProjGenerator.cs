7ef43c674a97d7e0d6834553b345f88b870c65e0
Jon Skeet <jonskeet@google.com>
2022-11-03 14:26:43 +0000

    chore: Update generator to use GAX 4.2.0

--- Google.Api.Generator/Generation/CsProjGenerator.cs
+++ Google.Api.Generator/Generation/CsProjGenerator.cs
@@ -22,7 +22,7 @@ namespace Google.Api.Generator.Generation
 {
     internal static class CsProjGenerator
     {
-        private const string GaxGrpcVersion = "[4.1.1, 5.0.0)";
+        private const string GaxGrpcVersion = "[4.2.0, 5.0.0)";
         private const string GrpcCoreVersion = "[2.46.3, 3.0.0)";
         private const string LroVersion = "[3.0.0, 4.0.0)";
         private const string IamVersion = "[3.0.0, 4.0.0)";
