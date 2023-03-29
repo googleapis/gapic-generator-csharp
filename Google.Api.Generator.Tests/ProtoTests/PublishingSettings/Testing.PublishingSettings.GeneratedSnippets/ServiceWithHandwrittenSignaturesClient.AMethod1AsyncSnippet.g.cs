d0c0897489d7527be514d0babac4a4dc499b0301
Jon Skeet <jonskeet@google.com>
2023-03-29 12:59:13 +0100

    tests: Generated code from publishing settings tests
    The settings mean:
    
    - We have RenamedServiceNameClient instead of OriginalServiceNameClient
    - We don't generate ServiceWithHandwrittenSignaturesClient.AMethod(string) or async equivalents, but we do call them from snippets
    - We use the common location resource name type in ResourceRequest (ParentAsCommonLocationName property)
    - We generated RenamedDatabaseName instead of OriginalDatabaseName (and have DatabaseAsRenamedDatabaseName in ResourceRequest)
    
    Further tested the impact by changing each setting one at a time: the tests fail if any setting is changed.

---
+++ Google.Api.Generator.Tests/ProtoTests/PublishingSettings/Testing.PublishingSettings.GeneratedSnippets/ServiceWithHandwrittenSignaturesClient.AMethod1AsyncSnippet.g.cs
@@ -0,0 +1,44 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Generated code. DO NOT EDIT!
+
+namespace Testing.PublishingSettings.Snippets
+{
+    // [START unknown_generated_ServiceWithHandwrittenSignatures_AMethod_async_flattened1]
+    using System.Threading.Tasks;
+    using Testing.PublishingSettings;
+
+    public sealed partial class GeneratedServiceWithHandwrittenSignaturesClientSnippets
+    {
+        /// <summary>Snippet for AMethodAsync</summary>
+        /// <remarks>
+        /// This snippet has been automatically generated and should be regarded as a code template only.
+        /// It will require modifications to work:
+        /// - It may require correct/in-range values for request initialization.
+        /// - It may require specifying regional endpoints when creating the service client as shown in
+        ///   https://cloud.google.com/dotnet/docs/reference/help/client-configuration#endpoint.
+        /// </remarks>
+        public async Task AMethod1Async()
+        {
+            // Create client
+            ServiceWithHandwrittenSignaturesClient serviceWithHandwrittenSignaturesClient = await ServiceWithHandwrittenSignaturesClient.CreateAsync();
+            // Initialize request argument(s)
+            string string1 = "";
+            // Make the request
+            Response response = await serviceWithHandwrittenSignaturesClient.AMethodAsync(string1);
+        }
+    }
+    // [END unknown_generated_ServiceWithHandwrittenSignatures_AMethod_async_flattened1]
+}
---
+++ Google.Api.Generator.Tests/ProtoTests/PublishingSettings/Testing.PublishingSettings.GeneratedSnippets/ServiceWithHandwrittenSignaturesClient.AMethod2Snippet.g.cs
@@ -0,0 +1,44 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Generated code. DO NOT EDIT!
+
+namespace Testing.PublishingSettings.Snippets
+{
+    // [START unknown_generated_ServiceWithHandwrittenSignatures_AMethod_sync_flattened2]
+    using Testing.PublishingSettings;
+
+    public sealed partial class GeneratedServiceWithHandwrittenSignaturesClientSnippets
+    {
+        /// <summary>Snippet for AMethod</summary>
+        /// <remarks>
+        /// This snippet has been automatically generated and should be regarded as a code template only.
+        /// It will require modifications to work:
+        /// - It may require correct/in-range values for request initialization.
+        /// - It may require specifying regional endpoints when creating the service client as shown in
+        ///   https://cloud.google.com/dotnet/docs/reference/help/client-configuration#endpoint.
+        /// </remarks>
+        public void AMethod2()
+        {
+            // Create client
+            ServiceWithHandwrittenSignaturesClient serviceWithHandwrittenSignaturesClient = ServiceWithHandwrittenSignaturesClient.Create();
+            // Initialize request argument(s)
+            string string1 = "";
+            string string2 = "";
+            // Make the request
+            Response response = serviceWithHandwrittenSignaturesClient.AMethod(string1, string2);
+        }
+    }
+    // [END unknown_generated_ServiceWithHandwrittenSignatures_AMethod_sync_flattened2]
+}
