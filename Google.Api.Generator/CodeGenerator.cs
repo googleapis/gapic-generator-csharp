96ceec423f0b8475e475603ef6ca595d61dd68d3
Amanda Tarafa Mas <atarafamas@google.com>
2023-08-09 15:03:50 -0700

    chore: Remove unnecessary dependencies.
    These are left-over from when we generated unit tests.

--- Google.Api.Generator/CodeGenerator.cs
+++ Google.Api.Generator/CodeGenerator.cs
@@ -71,8 +71,6 @@ namespace Google.Api.Generator
             { typeof(Google.Protobuf.WellKnownTypes.Any).Namespace, "wkt" },
             { typeof(Google.LongRunning.Operation).Namespace, "lro" },
             { typeof(Google.Protobuf.ByteString).Namespace, "proto" },
-            { typeof(Moq.Mock).Namespace, "moq" },
-            { typeof(Xunit.Assert).Namespace, "xunit" },
         };
 
         /// <summary>
