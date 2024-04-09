d2f58ad5e10aba51346e0d3520fd44f03bbd9e40
Jon Skeet <jonskeet@google.com>
2024-04-09 14:52:13 +0100

    tests: Reinstate DI extensions and metadata in publishing settings tests
    (These shouldn't have been removed, as settings can affect these
    classes.)
    
    Fixes b/332845188

---
+++ Google.Api.Generator.Tests/ProtoTests/PublishingSettings/Testing.PublishingSettings/ServiceCollectionExtensions.g.cs
@@ -0,0 +1,210 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Generated code. DO NOT EDIT!
+
+#pragma warning disable CS8981
+using gaxgrpc = Google.Api.Gax.Grpc;
+using lro = Google.LongRunning;
+using gpr = Google.Protobuf.Reflection;
+using sys = System;
+using scg = System.Collections.Generic;
+using tp = Testing.PublishingSettings;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>Static class to provide extension methods to configure API clients.</summary>
+    public static partial class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds a singleton <see cref="tp::RenamedServiceNameClient"/> to <paramref name="services"/>.
+        /// </summary>
+        /// <param name="services">
+        /// The service collection to add the client to. The services are used to configure the client when requested.
+        /// </param>
+        /// <param name="action">
+        /// An optional action to invoke on the client builder. This is invoked before services from
+        /// <paramref name="services"/> are used.
+        /// </param>
+        public static IServiceCollection AddRenamedServiceNameClient(this IServiceCollection services, sys::Action<tp::RenamedServiceNameClientBuilder> action = null) =>
+            services.AddSingleton(provider =>
+            {
+                tp::RenamedServiceNameClientBuilder builder = new tp::RenamedServiceNameClientBuilder();
+                action?.Invoke(builder);
+                return builder.Build(provider);
+            });
+
+        /// <summary>
+        /// Adds a singleton <see cref="tp::RenamedServiceNameClient"/> to <paramref name="services"/>.
+        /// </summary>
+        /// <param name="services">
+        /// The service collection to add the client to. The services are used to configure the client when requested.
+        /// </param>
+        /// <param name="action">
+        /// An optional action to invoke on the client builder. This is invoked before services from
+        /// <paramref name="services"/> are used.
+        /// </param>
+        public static IServiceCollection AddRenamedServiceNameClient(this IServiceCollection services, sys::Action<sys::IServiceProvider, tp::RenamedServiceNameClientBuilder> action) =>
+            services.AddSingleton(provider =>
+            {
+                tp::RenamedServiceNameClientBuilder builder = new tp::RenamedServiceNameClientBuilder();
+                action?.Invoke(provider, builder);
+                return builder.Build(provider);
+            });
+
+        /// <summary>
+        /// Adds a singleton <see cref="tp::ServiceWithApiVersionClient"/> to <paramref name="services"/>.
+        /// </summary>
+        /// <param name="services">
+        /// The service collection to add the client to. The services are used to configure the client when requested.
+        /// </param>
+        /// <param name="action">
+        /// An optional action to invoke on the client builder. This is invoked before services from
+        /// <paramref name="services"/> are used.
+        /// </param>
+        public static IServiceCollection AddServiceWithApiVersionClient(this IServiceCollection services, sys::Action<tp::ServiceWithApiVersionClientBuilder> action = null) =>
+            services.AddSingleton(provider =>
+            {
+                tp::ServiceWithApiVersionClientBuilder builder = new tp::ServiceWithApiVersionClientBuilder();
+                action?.Invoke(builder);
+                return builder.Build(provider);
+            });
+
+        /// <summary>
+        /// Adds a singleton <see cref="tp::ServiceWithApiVersionClient"/> to <paramref name="services"/>.
+        /// </summary>
+        /// <param name="services">
+        /// The service collection to add the client to. The services are used to configure the client when requested.
+        /// </param>
+        /// <param name="action">
+        /// An optional action to invoke on the client builder. This is invoked before services from
+        /// <paramref name="services"/> are used.
+        /// </param>
+        public static IServiceCollection AddServiceWithApiVersionClient(this IServiceCollection services, sys::Action<sys::IServiceProvider, tp::ServiceWithApiVersionClientBuilder> action) =>
+            services.AddSingleton(provider =>
+            {
+                tp::ServiceWithApiVersionClientBuilder builder = new tp::ServiceWithApiVersionClientBuilder();
+                action?.Invoke(provider, builder);
+                return builder.Build(provider);
+            });
+
+        /// <summary>
+        /// Adds a singleton <see cref="tp::ServiceWithHandwrittenSignaturesClient"/> to <paramref name="services"/>.
+        /// </summary>
+        /// <param name="services">
+        /// The service collection to add the client to. The services are used to configure the client when requested.
+        /// </param>
+        /// <param name="action">
+        /// An optional action to invoke on the client builder. This is invoked before services from
+        /// <paramref name="services"/> are used.
+        /// </param>
+        public static IServiceCollection AddServiceWithHandwrittenSignaturesClient(this IServiceCollection services, sys::Action<tp::ServiceWithHandwrittenSignaturesClientBuilder> action = null) =>
+            services.AddSingleton(provider =>
+            {
+                tp::ServiceWithHandwrittenSignaturesClientBuilder builder = new tp::ServiceWithHandwrittenSignaturesClientBuilder();
+                action?.Invoke(builder);
+                return builder.Build(provider);
+            });
+
+        /// <summary>
+        /// Adds a singleton <see cref="tp::ServiceWithHandwrittenSignaturesClient"/> to <paramref name="services"/>.
+        /// </summary>
+        /// <param name="services">
+        /// The service collection to add the client to. The services are used to configure the client when requested.
+        /// </param>
+        /// <param name="action">
+        /// An optional action to invoke on the client builder. This is invoked before services from
+        /// <paramref name="services"/> are used.
+        /// </param>
+        public static IServiceCollection AddServiceWithHandwrittenSignaturesClient(this IServiceCollection services, sys::Action<sys::IServiceProvider, tp::ServiceWithHandwrittenSignaturesClientBuilder> action) =>
+            services.AddSingleton(provider =>
+            {
+                tp::ServiceWithHandwrittenSignaturesClientBuilder builder = new tp::ServiceWithHandwrittenSignaturesClientBuilder();
+                action?.Invoke(provider, builder);
+                return builder.Build(provider);
+            });
+
+        /// <summary>
+        /// Adds a singleton <see cref="tp::ServiceWithMethodSettingsClient"/> to <paramref name="services"/>.
+        /// </summary>
+        /// <param name="services">
+        /// The service collection to add the client to. The services are used to configure the client when requested.
+        /// </param>
+        /// <param name="action">
+        /// An optional action to invoke on the client builder. This is invoked before services from
+        /// <paramref name="services"/> are used.
+        /// </param>
+        public static IServiceCollection AddServiceWithMethodSettingsClient(this IServiceCollection services, sys::Action<tp::ServiceWithMethodSettingsClientBuilder> action = null) =>
+            services.AddSingleton(provider =>
+            {
+                tp::ServiceWithMethodSettingsClientBuilder builder = new tp::ServiceWithMethodSettingsClientBuilder();
+                action?.Invoke(builder);
+                return builder.Build(provider);
+            });
+
+        /// <summary>
+        /// Adds a singleton <see cref="tp::ServiceWithMethodSettingsClient"/> to <paramref name="services"/>.
+        /// </summary>
+        /// <param name="services">
+        /// The service collection to add the client to. The services are used to configure the client when requested.
+        /// </param>
+        /// <param name="action">
+        /// An optional action to invoke on the client builder. This is invoked before services from
+        /// <paramref name="services"/> are used.
+        /// </param>
+        public static IServiceCollection AddServiceWithMethodSettingsClient(this IServiceCollection services, sys::Action<sys::IServiceProvider, tp::ServiceWithMethodSettingsClientBuilder> action) =>
+            services.AddSingleton(provider =>
+            {
+                tp::ServiceWithMethodSettingsClientBuilder builder = new tp::ServiceWithMethodSettingsClientBuilder();
+                action?.Invoke(provider, builder);
+                return builder.Build(provider);
+            });
+
+        /// <summary>
+        /// Adds a singleton <see cref="tp::ServiceWithResourcesClient"/> to <paramref name="services"/>.
+        /// </summary>
+        /// <param name="services">
+        /// The service collection to add the client to. The services are used to configure the client when requested.
+        /// </param>
+        /// <param name="action">
+        /// An optional action to invoke on the client builder. This is invoked before services from
+        /// <paramref name="services"/> are used.
+        /// </param>
+        public static IServiceCollection AddServiceWithResourcesClient(this IServiceCollection services, sys::Action<tp::ServiceWithResourcesClientBuilder> action = null) =>
+            services.AddSingleton(provider =>
+            {
+                tp::ServiceWithResourcesClientBuilder builder = new tp::ServiceWithResourcesClientBuilder();
+                action?.Invoke(builder);
+                return builder.Build(provider);
+            });
+
+        /// <summary>
+        /// Adds a singleton <see cref="tp::ServiceWithResourcesClient"/> to <paramref name="services"/>.
+        /// </summary>
+        /// <param name="services">
+        /// The service collection to add the client to. The services are used to configure the client when requested.
+        /// </param>
+        /// <param name="action">
+        /// An optional action to invoke on the client builder. This is invoked before services from
+        /// <paramref name="services"/> are used.
+        /// </param>
+        public static IServiceCollection AddServiceWithResourcesClient(this IServiceCollection services, sys::Action<sys::IServiceProvider, tp::ServiceWithResourcesClientBuilder> action) =>
+            services.AddSingleton(provider =>
+            {
+                tp::ServiceWithResourcesClientBuilder builder = new tp::ServiceWithResourcesClientBuilder();
+                action?.Invoke(provider, builder);
+                return builder.Build(provider);
+            });
+    }
+}
