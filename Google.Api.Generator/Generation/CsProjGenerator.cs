d31fdc6df132324ac5da01520d477699ad20a1b9
Amanda Tarafa Mas <atarafamas@google.com>
2025-01-28 00:09:20 -0800

    build: Update to latest GAX and Google.Apis

--- Google.Api.Generator/Generation/CsProjGenerator.cs
+++ Google.Api.Generator/Generation/CsProjGenerator.cs
@@ -23,7 +23,7 @@ namespace Google.Api.Generator.Generation
 {
     internal static class CsProjGenerator
     {
-        private const string GaxGrpcVersion = "[4.9.0, 5.0.0)";
+        private const string GaxGrpcVersion = "[4.10.0, 5.0.0)";
         private const string GrpcCoreVersion = "[2.46.6, 3.0.0)";
         private const string LroVersion = "[3.3.0, 4.0.0)";
         private const string IamVersion = "[3.3.0, 4.0.0)";
