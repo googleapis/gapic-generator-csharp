399d2cdc6d706c82959c187e3eeb859818784068
Robert Voinescu <robertvoinescu@google.com>
2026-06-10 20:54:11 +0000

    chore: Add script to run unit tests in CI

---
+++ rununittests.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This script:
+# - Initializes/updates git submodules (required for common protos)
+# - Builds the Generator solution in Release mode
+# - Runs unit tests for the Generator projects in Release mode
+
+set -ex
+
+# To avoid printing the dotnet CLI welcome message
+export DOTNET_NOLOGO=true
+
+echo "Initializing/updating git submodules"
+git submodule update --init --recursive
+
+echo "Building the solution in Release mode"
+dotnet build Generator.sln -c Release
+
+echo "Unit testing Generator projects in Release mode"
+# Using --no-build since we already built the whole solution above
+dotnet test --no-build -c Release Google.Api.Generator.Utils.Tests
+dotnet test --no-build -c Release Google.Api.Generator.Tests
+dotnet test --no-build -c Release Google.Api.Generator.Rest.Tests
+
+echo "Build and unit testing completed"
